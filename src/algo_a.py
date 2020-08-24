4efe25b35276427cbf677177cfbb382c905b84cd
James Sutton <james@algorithmia.com>
2020-08-24 15:05:00 -0500

    initial commit, exporting to Algorithmia

--- src/algo_a.py
+++ src/algo_a.py
@@ -1,6 +1,27 @@
 import Algorithmia
+import tensorflow as tf
+import shutil
 
-# API calls will begin at the apply() method, with the request body passed as 'input'
-# For more details, see algorithmia.com/developers/algorithm-development/languages
+client = Algorithmia.client()
+
+
+sm_path = "/tmp/saved_model"
+model_path = "data://network_anomaly_detection/models/modela_tf_23_aug2320.zip"
+
+def load():
+    local_path = client.file(model_path).getFile().name
+    shutil.unpack_archive(local_path, sm_path, format='zip')
+    model = tf.keras.models.load_model(sm_path, compile=False)
+    return model
+
+
 def apply(input):
-    return "hello {}".format(input)
+    tensor = tf.constant(input)
+    output = model.predict(tensor)
+    return output
+
+model = load()
+
+
+output = apply([[4.0, 3.0, 2.2, -1., 0.25]])
+print(output)
---
+++ src/model_def.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+import shutil
+import os
+
+class Model(tf.keras.Model):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.layer1 = layers.Dense(4, name="layer1", activation="relu")
+        self.layer2 = layers.Dense(4, name="layer2", activation="relu")
+        self.out = layers.Dense(1, name="output")
+
+    def call(self, inputs, training=None, mask=None):
+        o1 = self.layer1(inputs)
+        o2 = self.layer2(o1)
+        final = self.out(o2)
+        return final
+
+
+
+def main():
+    model = Model()
+    model.build((1, 5))
+    l = tf.constant([[4.0, 3.0, 2.2, -1., 0.25]], shape=(1, 5))
+    r = model.predict(l)
+    print(r)
+    model.save("/tmp/tf_model")
+    shutil.make_archive("/tmp/tf_model_example", 'zip', "/tmp/tf_model")
+
+
+main()
