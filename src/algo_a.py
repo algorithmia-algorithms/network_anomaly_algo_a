e07ac60f8c7c1ef8dc986aa41f5ef5147c9349d2
James Sutton <james@algorithmia.com>
2020-09-24 10:13:37 -0500

    removed non-functional TF logging

--- src/algo_a.py
+++ src/algo_a.py
@@ -1,10 +1,8 @@
 import Algorithmia
 import tensorflow as tf
-import logging
 import shutil
-import os
-from pathlib import Path
 from time import time
+
 client = Algorithmia.client()
 
 SM_PATH = "/tmp/saved_model"
@@ -14,36 +12,18 @@ LOGGING_FILE = "/tmp/logging_file"
 class TF_Logging():
     def __init__(self, logging_file):
         self.log_file = logging_file
-        self.get_logger()
         self.events = []
 
     def insert_event(self, message):
         event = {"timestamp": str(time()), "message": message}
         self.events.append(event)
 
-    def parse_logs(self):
-        with open(self.log_file, 'r') as f:
-            for line in f.readline():
-                elements = line.split(' - ')
-                timestamp = str(elements[0])
-                message = str(elements[-1])
-                event = {"timestamp": timestamp, "message": message}
-                self.events.append(event)
-        os.remove(self.log_file)
-        open(self.log_file, 'w').close()
 
     def get_events(self):
         events = self.events
         self.events = []
         return events
 
-    def get_logger(self):
-        log = logging.getLogger("tensorflow")
-        log.setLevel("DEBUG")
-        formatter = logging.Formatter('%(created)f - %(levelname)s - %(message)s')
-        fh = logging.FileHandler(self.log_file)
-        fh.setFormatter(formatter)
-        log.addHandler(fh)
 
 
 
@@ -55,17 +35,14 @@ def load():
 
 
 def apply(input):
+    LOGGER = TF_Logging(LOGGING_FILE)
     LOGGER.insert_event("input to ALGO_A is: {}".format(input))
     tensor = tf.constant(input)
     outcome = MODEL.predict(tensor).tolist()[0][0]
-    LOGGER.parse_logs()
     LOGGER.insert_event("outcome predicted by ALGO_A is: {}".format(outcome))
-    events = LOGGER.get_events()
-    output = {"outcome": outcome, "events": events}
+    output = {"outcome": outcome, "events": LOGGER.get_events()}
     return output
 
-
-LOGGER = TF_Logging(LOGGING_FILE)
 MODEL = load()
 
 if __name__ == "__main__":
